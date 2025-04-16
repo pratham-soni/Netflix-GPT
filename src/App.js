467c408f9380fccd21748c339f16180bf0342de5
pratham-soni <spratham304@gmail.com>
2025-04-16 10:47:23 +0530

    settimg up git repo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello World !!</p>
+        <h1>This is edited h1 tag</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
